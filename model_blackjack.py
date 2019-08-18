c6c64a760a7203442a2039a649b2597140a37301
Mitja Mandić <mitja.mandic@gmail.com>
2019-08-18 10:54:56 +0200

    razred kup in igralec, problem vedno znova zmeša kup in se karte ponavljajo

--- model_blackjack.py
+++ model_blackjack.py
@@ -1,19 +1,42 @@
 import random
 DENAR = 100   
 STAVA = 0
+BARVE = ['Srca', 'Kara', 'Pik', 'Križ']
 
 class Karta:
-    FACES = {11: 'Jack', 12: 'Queen', 13: 'King', 14: 'Ace'}
-    
-    def __init__(self, barva, vrednost):
+    def __init__(self, barva, stevilo):
         self.barva = barva
-        self.vrednost = vrednost
-    
-    def __str__(self):
-        return "{0}, {1}".format(self.barva, self.vrednost)
-vrednosti = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
-barve = [1,2,3,4]
+        self.stevilo = stevilo
 
-deck = [Karta(barva, vrednost) for barva in range(1,4) for vrednost in range(1, 14)]
-random.shuffle(deck)
-for karta in deck: print(str(karta))
+    def __repr__(self):
+        return f'Karta({self.barva}, {self.stevilo})'
+    
+    def vredost(self):
+        if self.stevilo in range(1,11):
+            self.vrednost = self.stevilo
+        elif self.stevilo in range(11,14):
+            self.vrednost == 10
+
+class Kup:
+    def __init__(self):
+        self.kup = None
+    
+    def zmešaj(self):
+        self.kup = [Karta(barva, stevilo) for barva in BARVE for stevilo in range(1, 14)]
+        random.shuffle(self.kup)
+        return self.kup
+
+    def __repr__(self):
+        return f'{self.kup}'
+
+class Igralec:
+    def __init__(self):
+        self.roka = []
+    def hit(self):
+        self.roka.append(Kup().zmešaj()[1])
+
+    def __str__(self):
+        return f'igralec, {self.roka}'
+m = Igralec()
+m.hit()
+print(str(m))
