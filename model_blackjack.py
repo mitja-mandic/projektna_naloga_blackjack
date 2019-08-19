01074264f8af3cdc9465ebfe71dcb7478d68bd09
mandic <49949550+mitja-mandic@users.noreply.github.com>
2019-08-19 23:19:52 +0200

    vrednost še vedno ne dela

--- model_blackjack.py
+++ model_blackjack.py
@@ -6,27 +6,27 @@ ZMAGA = 'w'
 PORAZ = 'x'
 
 class Karta:
-    def __init__(self, barva, stevilo):
+    def __init__(self, barva, stevilo, vrednost):
         self.barva = barva
         self.stevilo = stevilo
-        self.vrednost = 0
+        self.vrednost = vrednost
+        
+ 
+
 
     def __repr__(self):
-        return f'Karta({self.barva}, {self.stevilo})'
-    
-    def vrednost(self):
-        if self.stevilo not in range(2,11):
-            self.vrednost = 10
-        elif self.stevilo == 'A':
-            self.vrednost = 1
-        else:
-            self.vrednost = self.stevilo
+        return f'Karta({self.barva}, {self.stevilo}, {self.vrednost})'
+
+
+
+
+
 class Kup:
     def __init__(self):
         self.kup = None
     
     def zmešaj(self):
-        self.kup = [Karta(barva, stevilo) for barva in BARVE for stevilo in [2,3,4,5,6,7,8,9,10,'J','Q','K','A']]
+        self.kup = [Karta(barva, stevilo, vrednost) for barva in BARVE for stevilo in [2,3,4,5,6,7,8,9,10,'J','Q','K','A'] for vrednost in range(1,10)]
         random.shuffle(self.kup)
         return self.kup
     
@@ -40,6 +40,7 @@ class Igra:
         self.roka1 = []
         self.roka2 = []
         self.kup = Kup().zmešaj()
+
 
 #    def odstrani_dvojnike(self, seznam):
 #        for x in seznam:
@@ -63,24 +64,26 @@ class Igra:
         self.roka1.append(self.kup[0])
         self.kup.remove(self.roka1[-1])
     
-    def diler(self):
+    #def vrednost(self, seznam):    
+    #    if karta.stevilo not in range(2,11):
+    #        karta.vrednost = 10
+    #    elif karta.stevilo == 'A':
+    #        karta.vrednost = 1
+    #    else:
+    #        karta.vrednost = karta.stevilo
+    
+    def doloci_vrednost_roke(self, seznam):
         vsota = 0
-        for karta in self.dealer:
-            vsota += karta.vrednost()
-            if vsota <= 16:
-                self.dealer.append(self.kup[0])
-            else:
-                pass
+        for karta in seznam:
+            vsota += karta.vrednost
 
-
-
-
-
-
-
+        return vsota
     def __repr__(self):
-        return f'{self.dealer},{self.roka1}, {len(self.kup)}'
+        return f'{self.roka1}'
+
 m = Igra()
 m.deal()
-m.diler()
-print(str(m))
+m.vrednost(m.roka1)
+print(repr(m))
+
+print(str(m.doloci_vrednost_roke(m.roka1)))
