79d03f454116961c039d8500628426d153ead08e
Mitja Mandić <mitja.mandic@gmail.com>
2019-08-19 18:18:28 +0200

    izboljšan dealer, vrednost ne dela

--- model_blackjack.py
+++ model_blackjack.py
@@ -9,6 +9,7 @@ class Karta:
     def __init__(self, barva, stevilo):
         self.barva = barva
         self.stevilo = stevilo
+        self.vrednost = 0
 
     def __repr__(self):
         return f'Karta({self.barva}, {self.stevilo})'
@@ -16,12 +17,10 @@ class Karta:
     def vrednost(self):
         if self.stevilo not in range(2,11):
             self.vrednost = 10
-            return self.vrednost
         elif self.stevilo == 'A':
-            self.vrednost == 1
-            return int(self.vrednost)
+            self.vrednost = 1
         else:
-            return int(self.stevilo)
+            self.vrednost = self.stevilo
 class Kup:
     def __init__(self):
         self.kup = None
@@ -30,6 +29,9 @@ class Kup:
         self.kup = [Karta(barva, stevilo) for barva in BARVE for stevilo in [2,3,4,5,6,7,8,9,10,'J','Q','K','A']]
         random.shuffle(self.kup)
         return self.kup
+    
+    def __repr__(self):
+        return f'{self.kup}'
 
 
 class Igra:
@@ -39,38 +41,46 @@ class Igra:
         self.roka2 = []
         self.kup = Kup().zmešaj()
 
-    def odstrani_dvojnike(self):
-        for karta in self.roka1:
-            self.kup.remove(karta)
-        return self.kup
+#    def odstrani_dvojnike(self, seznam):
+#        for x in seznam:
+#            self.kup.remove(x)
     
     def deal(self):
-        že_podeljene = []
-        for karta in self.kup:
-            if len(self.dealer) < 2 and karta not in že_podeljene :
-                self.dealer.append(karta)
-                že_podeljene.append(karta)
-            else:
-                continue      
-        self.kup.odstrani_dvojnike()
+        že_podeljene = []    
         for karta in self.kup:
             if len(self.roka1) < 2 and karta not in že_podeljene :
                 self.roka1.append(karta)
                 že_podeljene.append(karta)
+            elif karta not in self.roka1 and karta not in že_podeljene and len(self.dealer) < 2:
+                self.dealer.append(karta)
+                že_podeljene.append(karta)
             else:
                 continue
-        return self.roka1
-        if sum(karta.vrednost() for karta in self.roka1) == 21:
-            return ZMAGA
-    
+        for karta in že_podeljene:
+            self.kup.remove(karta)
     
     def hit(self):
         self.roka1.append(self.kup[0])
-        return self.roka1
+        self.kup.remove(self.roka1[-1])
+    
+    def diler(self):
+        vsota = 0
+        for karta in self.dealer:
+            vsota += karta.vrednost()
+            if vsota <= 16:
+                self.dealer.append(self.kup[0])
+            else:
+                pass
+
+
+
+
+
+
+
     def __repr__(self):
-        return f'{self.dealer},{self.roka}'
+        return f'{self.dealer},{self.roka1}, {len(self.kup)}'
 m = Igra()
 m.deal()
-print(repr(m))
-    
-    
+m.diler()
+print(str(m))
