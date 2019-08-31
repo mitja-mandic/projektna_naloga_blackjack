2d2817f046cd8bdc8a14dc7fa6efa7dd8278dd5b
mandic <49949550+mitja-mandic@users.noreply.github.com>
2019-08-31 18:44:00 +0200

    delo na splitu, nisem ziher če dela konec igre

--- model_blackjack.py
+++ model_blackjack.py
@@ -5,7 +5,7 @@ ZMAGA = 'wW'
 PORAZ = 'xx'
 ZMAGA_RUNDE = 'w'
 PORAZ_RUNDE = 'x'
-
+PUSH = 'p'
 class Karta:
     def __init__(self, barva, stevilo):
         self.barva = barva
@@ -40,6 +40,7 @@ class Igra:
     def stava(self, znesek):
         global DENAR
         if znesek <= DENAR and znesek != 0:
+            DENAR -= znesek
             self.znesek += znesek
             return True
         else:
@@ -47,30 +48,22 @@ class Igra:
 
     def deal(self, roka):
         že_podeljene = []    
-        if self.stava:
-            for karta in self.kup:
-                if len(roka) < 2 and karta not in že_podeljene :
-                    roka.append(karta)
-                    že_podeljene.append(karta)
-                elif karta not in roka and karta not in že_podeljene and len(self.dealer) < 2:
-                    self.dealer.append(karta)
-                    že_podeljene.append(karta)
-                else:
-                    continue
+        #if self.stava:
+        for karta in self.kup:
+            if len(roka) < 2 and karta not in že_podeljene :
+                roka.append(karta)
+                že_podeljene.append(karta)
+            elif karta not in roka and karta not in že_podeljene and len(self.dealer) < 2:
+                self.dealer.append(karta)
+                že_podeljene.append(karta)
+            else:
+                continue
         
         for karta in že_podeljene:
             self.kup.remove(karta)
-    
-    def deal_split(self):
-        if self.split():
-            for roka in self.roke:
-                self.deal(roka)
-        else:
-            pass
-
         
     def hit(self, igralec):
-        if self.stava:
+        if self.stava:    
             igralec.append(self.kup[0])
             self.kup.remove(igralec[-1])
 
@@ -85,12 +78,11 @@ class Igra:
                 self.vrednost_roke += karta.stevilo
         return self.vrednost_roke
 
-    def soft_hand(self):
-        for i, s in enumerate(self.roka1):
+    def soft_hand(self, roka):
+        for i, s in enumerate(roka):
             if s.stevilo == 'A':
-                self.roka1[i] = Karta(barva=s.barva,stevilo = 11)
-                #print('menjam as za 11')
-            return self.roka1
+                roka[i] = Karta(barva=s.barva,stevilo = 11)
+                return roka
 
     def dealers_play(self):
         while self.doloci_vrednost_roke(self.dealer) <= 17:
@@ -101,18 +93,27 @@ class Igra:
         if self.roka1[0].stevilo == self.roka1[1].stevilo:
             self.roka2.append(self.roka1[1])
             self.roka1.pop()
+            self.znesek *= 2
+            for roka in self.roke:
+                self.deal(roka)
+            return True
+        else:
+            return False
 
-    def konec_igre(self):
+    def konec_igre(self, roka):
         global DENAR
-        if DENAR <=  0:
+        if DENAR <= 0:
             return PORAZ
         elif DENAR == 300:
             return ZMAGA
-        elif self.doloci_vrednost_roke(self.roka1) > 21:
+        elif (self.doloci_vrednost_roke(self.dealer) < 21) and (self.doloci_vrednost_roke(roka) < 21) and self.doloci_vrednost_roke(roka) == self.doloci_vrednost_roke(self.dealer):
+            self.znesek = 0
+            return PUSH
+        elif self.doloci_vrednost_roke(roka) > 21:
             DENAR -= self.znesek
             self.znesek = 0
             return PORAZ_RUNDE
-        elif self.doloci_vrednost_roke(self.roka1) == 21:
+        elif self.doloci_vrednost_roke(roka) == 21:
             DENAR += self.znesek
             self.znesek = 0
             return ZMAGA_RUNDE
@@ -124,29 +125,53 @@ class Igra:
             DENAR += self.znesek
             self.znesek = 0
             return ZMAGA_RUNDE
-        elif (self.doloci_vrednost_roke(self.dealer) < 21) and (self.doloci_vrednost_roke(self.roka1) < 21) and (self.doloci_vrednost_roke(self.roka1) < self.doloci_vrednost_roke(self.dealer)):
+        elif (self.doloci_vrednost_roke(self.dealer) < 21) and (self.doloci_vrednost_roke(roka) < 21) and (self.doloci_vrednost_roke(roka) < self.doloci_vrednost_roke(self.dealer)):
             DENAR -= self.znesek
             self.znesek = 0
             return PORAZ_RUNDE
-        elif (self.doloci_vrednost_roke(self.dealer) < 21) and (self.doloci_vrednost_roke(self.roka1) < 21) and (self.doloci_vrednost_roke(self.roka1) > self.doloci_vrednost_roke(self.dealer)):
+        elif (self.doloci_vrednost_roke(self.dealer) < 21) and (self.doloci_vrednost_roke(roka) < 21) and (self.doloci_vrednost_roke(roka) > self.doloci_vrednost_roke(self.dealer)):
             DENAR += self.znesek
             self.znesek = 0
             return ZMAGA_RUNDE
-
+        
     def reset(self):
         self.dealer = []
         self.roka1 = []
+        self.roka2 = []
+        self.roke = [self.roka1, self.roka2]
         self.kup = Kup().zmešaj()
         self.deal(self.roka1)
+    
+    def preveri_konec(self):
+        rezultati = []
+        for roka in self.roke:
+            if roka != []:
+                if self.konec_igre(roka) == ZMAGA_RUNDE:
+                    rezultati.append(1)
+                elif self.konec_igre(roka) == PORAZ_RUNDE:
+                    rezultati.append(-2)
+                elif self.konec_igre(roka) == PUSH:
+                    rezultati.append(0)
+        if sum(rezultati) == 0:
+            return PUSH
+        elif sum(rezultati) < 0:
+            return PORAZ_RUNDE
+        else:
+            return ZMAGA_RUNDE
+    
+    
+    
     def __repr__(self):
-        return f'{self.roka1}, {self.doloci_vrednost_roke(self.roka1)}, {self.roka2},{self.roke}'
+        return f'{self.roka1},{self.roka2},{self.roke}'
     #return f'{self.roka1},{self.dealer},{self.doloci_vrednost_roke(self.roka1)},{self.doloci_vrednost_roke(self.dealer)}'
 
-m = Igra()
-m.deal(m.roka1)
-print(repr(m))
-m.split()
-print(repr(m))
+#m = Igra()
+#m.deal(m.roka1)
+#print(repr(m))
+#m.split()
+#print(repr(m))
+
+
 
 def nova_igra():
     return Igra()
--- tekstovni_vmesnik_blackjack.py
+++ tekstovni_vmesnik_blackjack.py
@@ -1,7 +1,7 @@
 import model_blackjack
 lojtrice = "######################################\n"
 
-igra = model_blackjack.nova_igra()
+
 
 
 
@@ -10,12 +10,12 @@ def izpis_zmage(igra):
     return tekst
 
 def izpis_poraza(igra):
-    tekst = lojtrice + 'Izgubili ste. Dealer ima je imel v roki {0}, vi pa {1} Več sreče prihodnjič!'.format(igra.dealer, igra.roka1)
+    tekst = lojtrice + 'Izgubili ste. Dealer je imel v roki {0}, vi pa {1} Več sreče prihodnjič!'.format(igra.dealer, igra.roke)
     return tekst
 
 def izpis_igre(igra):
     tekst = lojtrice + ('''V roki imate {0} in vsota vrednosti vaših kart je {1} \ndealer ima v roki 
-    {2} \n imate še {3} žetonov, stavili ste {4}.''').format(igra.roka1, igra.doloci_vrednost_roke(igra.roka1), igra.dealer[:-1], model_blackjack.DENAR, igra.znesek)
+    {2} \n imate še {3} žetonov, stavili ste {4}.''').format(igra.roke, igra.doloci_vrednost_roke(igra.roke[0]), igra.dealer[:-1], model_blackjack.DENAR, igra.znesek)
     return tekst
 
 def izpis_poslovila(igra):
@@ -23,93 +23,146 @@ def izpis_poslovila(igra):
     return tekst
 
 def izpis_poraz_runde(igra):
-    tekst =lojtrice + 'Ta krog ste izgubili. V roki ste imeli {0},\ndealer pa {1}. Na računu imate še {2}.'.format(igra.roka1, igra.dealer, model_blackjack.DENAR)
+    tekst =lojtrice + 'Ta krog ste izgubili. V roki ste imeli {0},\ndealer pa {1}. Na računu imate še {2}.'.format(igra.roke, igra.dealer, model_blackjack.DENAR)
     return tekst
 
 def izpis_zmage_runde(igra):
-    tekst = lojtrice + 'Čestitamo, ta krog ste zmagali. V roki ste imeli {0},\ndealer pa {1}. Na računu imate sedaj {2}.'.format(igra.roka1, igra.dealer, model_blackjack.DENAR)
+    tekst = lojtrice + 'Čestitamo, ta krog ste zmagali. V roki ste imeli {0},\ndealer pa {1}. Na računu imate sedaj {2}.'.format(igra.roke, igra.dealer, model_blackjack.DENAR)
     return tekst
 
 def trenutno_stanje(igra):
-    tekst = lojtrice + f'V roki imate {igra.roka1}.'
+    tekst = lojtrice + f'V roki imate {igra.roke}.'
     return tekst
 
 def pozeni_vmesnik():
-    
-        
-    igra.deal()
-
+    igra = model_blackjack.nova_igra()
+    igra.deal(igra.roke[0])
     while True:
+        
         stava = int(input("Koliko stavite?"))
-    
         if not igra.stava(stava):
             print('Nimate toliko denarja.')
             break
-        
-        if model_blackjack.DENAR <= 0:
-            print(izpis_poraza)
+        if model_blackjack.DENAR < 0:
+            print(izpis_poraza(igra))
             break
         
         print(izpis_igre(igra))
 
-        poteza = input("hit ali stand?")
-        if poteza == 'hit':
-            while poteza == 'hit':
-                igra.hit(igra.roka1)
-                if igra.doloci_vrednost_roke(igra.roka1) < 21:
-                    print(f'V roki imate {igra.roka1} in vrednost tega je {igra.doloci_vrednost_roke(igra.roka1)}')
-                    poteza = input("hit ali stand?")
-                    if poteza == 'stand':
-                        break
-                elif igra.doloci_vrednost_roke(igra.roka1) == 21:
-                    break
-                elif igra.doloci_vrednost_roke(igra.roka1) > 21:
-                    break
+        if igra.roka1[0].stevilo == igra.roka1[1].stevilo:
+            split = input('split? s tem tudi podvojite svojo stavo.')
+            if split == 'ja':
+                igra.split()
+            else:
+                pass
+        print(izpis_igre(igra))
 
-        elif poteza == 'stand':
-            pass
+        poteza = input("hit ali stand?")
         
-        for karta in igra.roka1:
-            if karta.stevilo == 'A':
-                soft_hand = int(input('V roki imate asa. 1 ali 11>'))
-                if soft_hand == int(11):                            
-                    igra.soft_hand()
-                    #print(trenutno_stanje(igra))
-                    continue
-                else:
-                    pass
+        if poteza == 'hit':
+            i = 0
+            while poteza == 'hit':
+                igra.hit(igra.roke[i])
+                #print(trenutno_stanje(igra))
+                if igra.doloci_vrednost_roke(igra.roke[i]) < 21:
+                    print(f'V roki imate {igra.roke} in vrednost tega je {igra.doloci_vrednost_roke(igra.roke[i])}')
+                    poteza = input("hit ali stand?")
+                if poteza == 'stand':
+                    print('sedaj igrate še za drugo roko.')
+                    if igra.roke[1] != [] and i < 1:
+                        i += 1
+                        poteza = input('hit ali stand?')
+                    else:
+                        break
+                elif igra.doloci_vrednost_roke(igra.roke[i]) == 21:
+                    break
+                elif igra.doloci_vrednost_roke(igra.roke[i]) > 21:
+                    break
+                
+                
+                
+                
+                
+            #    if poteza == 'stand':
+            #        if igra.roke[1] != []:
+            #            i += 1
+            #            continue
+            #        else:
+            #            break
+                
+
+        
+        
+        
+        
+        
+        #for roka in igra.roke:
+        #    if roka != []:
+        #        while poteza == 'hit':
+        #            igra.hit(roka)
+        #            if igra.doloci_vrednost_roke(roka) < 21:
+        #                print(f'V roki imate {igra.roke} in vrednost tega je {igra.doloci_vrednost_roke(roka)}')
+        #                poteza = input("hit ali stand?")
+        #            if poteza == 'stand':
+        #                break
+        #            elif igra.doloci_vrednost_roke(roka) == 21:
+        #                break
+        #            elif igra.doloci_vrednost_roke(roka) > 21:
+        #                break
+        #    else:
+        #        break
+
+        if poteza == 'stand':
+            pass
+        print(izpis_igre(igra))
+        
+        for roka in igra.roke:
+            for karta in roka:
+                if karta.stevilo == 'A':
+                    soft_hand = int(input('V roki imate asa. 1 ali 11>'))
+                    if soft_hand == int(11):                            
+                        igra.soft_hand(roka)
+                        #print(trenutno_stanje(igra))
+                        continue
+                    else:
+                        pass
 
 
         igra.dealers_play()
-        
-        #print(izpis_igre(igra))
-        
-        if igra.konec_igre() == model_blackjack.PORAZ:
+
+
+
+        if model_blackjack.DENAR == 0:
             print(igra.roka1)
             print(izpis_poslovila(igra))
             print(izpis_poraza(igra))
             break
+        #elif igra.preveri_konec() == model_blackjack.ZMAGA:
+        #    print(izpis_zmage(igra))
+        #    print(izpis_poslovila(igra))
+        #    break
         
-        elif igra.konec_igre() == model_blackjack.ZMAGA:
-            print(izpis_zmage(igra))
-            print(izpis_poslovila(igra))
-            break
-        
-        elif igra.konec_igre() == model_blackjack.PORAZ_RUNDE:
+        elif igra.preveri_konec() == model_blackjack.PORAZ_RUNDE:
             print(izpis_poraz_runde(igra))
             odg = input('Ali želite igrati naprej?')
             if odg.lower() == 'ja' or odg.lower() == 'da':
                 igra.reset()
-                igra.deal()
             else:
                 print(izpis_poslovila(igra))
                 break
-        elif igra.konec_igre() == model_blackjack.ZMAGA_RUNDE:
+        elif igra.preveri_konec() == model_blackjack.ZMAGA_RUNDE:
             print(izpis_zmage_runde(igra))
             odg = input('Ali želite igrati naprej?')
             if odg.lower() == 'ja' or odg.lower() == 'da':
                 igra.reset()
-                igra.deal()
+            else:
+                print(izpis_poslovila(igra))
+                break
+        elif igra.preveri_konec() == model_blackjack.PUSH:
+            print('Imate enako kot dealer, denar se vam vrne.')
+            odg = input('Ali želite igrati naprej?')
+            if odg.lower() == 'ja' or odg.lower() == 'da':
+                igra.reset()
             else:
                 print(izpis_poslovila(igra))
                 break
